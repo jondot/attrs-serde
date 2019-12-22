90552f4490433ca56db3523716b590d9aa46be74
Tom Sender <tomsender@gmail.com>
2019-12-22 15:16:32 +0200

    Support default values in ser and des

--- tests/test_dict.py
+++ tests/test_dict.py
@@ -1,8 +1,8 @@
-from toolz import pipe
 from attrs_serde import serde
 from attr import attrs, attrib
 
 person_dict = {"contact": {"personal": {"name": "John"}, "phone": "555-112233"}}
+city_dict = {"name": "Tel-Aviv", 'zipcode': "6100000"}
 
 name_path = ["contact", "personal", "name"]
 phone_path = ["contact", "phone"]
@@ -20,6 +20,13 @@ class Person(object):
 class Contact(Person):
     zip_code = attrib(default=56000, metadata={"to": phone_path})
     address = attrib(default="Abby Rd.")
+
+
+@serde
+@attrs
+class City(object):
+    name = attrib()
+    zipcode = attrib()
 
 
 @serde(from_key="get", to_key="set")
@@ -45,6 +52,11 @@ def test_ser(snapshot):
     snapshot.assert_match(Person(name="Slash", phone="555-334455").to_dict())
 
 
+def test_ser_with_default_values(snapshot):
+    p_dict = City(name="Tel-Aviv", zipcode="6100000").to_dict()
+    snapshot.assert_match({"name": p_dict.get('name'), "zipcode": p_dict.get('zipcode')})
+
+
 def test_quirky_object(snapshot):
     c = Contact.from_dict(person_dict)
     snapshot.assert_match({"name": c.name, "phone": c.phone, "zip": c.zip_code})
@@ -52,3 +64,8 @@ def test_quirky_object(snapshot):
 
 def test_irrelevant_object(snapshot):
     c = Contact.from_dict({"irrelevant": True})
+
+
+def test_deser_with_default_values(snapshot):
+    p = City(name="Tel-Aviv", zipcode="6100000")
+    snapshot.assert_match({"name": p.name, "zipcode": p.zipcode})
